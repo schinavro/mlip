4d144f4c6af09c3c8fa431eab8fc4749a615e002
me <schinavro@gmail.com>
2022-09-30 17:54:16 +0900

    Indexing order fix

--- mlip/reann.py
+++ mlip/reann.py
@@ -1,12 +1,7 @@
-import time
-import copy
 import itertools
 import torch as tc
 from torch import nn
 from torch.nn.parameter import Parameter
-
-import numpy as np
-from ase import Atoms
 
 
 def compress_symbols(symbols):
@@ -53,10 +48,11 @@ def get_nn(symbols, positions, cell, pbc, cutoff=6., device='cpu'):
     nidxs, iidxs, jidxs, disps, dists
 
     """
-    A = len(positions)
+    # A = len(positions)
     icell = tc.linalg.pinv(cell)
     grid = tc.zeros(3).long().to(device=device)
-    grid[pbc] = ((2 * cutoff * tc.linalg.norm(icell, axis=0)).to(device=device).long() + 1)[pbc]
+    full = ((2*cutoff*tc.linalg.norm(icell, axis=0)).long() + 1)
+    grid[pbc] = full.to(device=device)[pbc]
 
     iidxs, jidxs, disps, dists, isymb, jsymb = [], [], [], [], [], []
     for n1, n2, n3 in itertools.product(range(0, grid[0] + 1),
@@ -113,10 +109,12 @@ def get_nn(symbols, positions, cell, pbc, cutoff=6., device='cpu'):
         disps.append(disp2[mask2])
         dists.append(dist2[mask2])
 
-    return tc.cat(iidxs), tc.cat(jidxs), tc.cat(isymb), tc.cat(jsymb), tc.cat(disps), tc.cat(dists)
+    return (tc.cat(iidxs), tc.cat(jidxs), tc.cat(isymb), tc.cat(jsymb),
+            tc.cat(disps), tc.cat(dists))
 
 
-def get_neighbors_info(symbols, positions, cells, pbcs, energyidx, crystalidx, cutoff=None, device='cpu'):
+def get_neighbors_info(symbols, positions, cells, pbcs, energyidx, crystalidx,
+                       cutoff=None, device='cpu'):
     """
     Parameters
     ----------
@@ -152,7 +150,8 @@ def get_neighbors_info(symbols, positions, cells, pbcs, energyidx, crystalidx, c
         crystali = totalidx[cmask]
         pbc, cell = pbcs[c], cells[c]
         # NN, NN, NNx3, NN
-        idx, jdx, isy, jsy, dsp, dst = get_nn(symbol, position, cell, pbc, cutoff=cutoff, device=device)
+        idx, jdx, isy, jsy, dsp, dst = get_nn(symbol, position, cell, pbc,
+                                              cutoff=cutoff, device=device)
         iidx.append(crystali[idx])
         # iidx.append(idx)
         isym.append(isy)
@@ -163,7 +162,103 @@ def get_neighbors_info(symbols, positions, cells, pbcs, energyidx, crystalidx, c
         disp.append(dsp)
         dist.append(dst)
 
-    return tc.cat(iidx), tc.cat(jidx), tc.cat(isym), tc.cat(jsym), tc.cat(disp), tc.cat(dist)
+    return (tc.cat(iidx), tc.cat(jidx), tc.cat(isym), tc.cat(jsym),
+            tc.cat(disp), tc.cat(dist))
+
+
+def cutoff_function(dist, rcut):
+    """
+    Parameters
+    ----------
+    dist: NTA
+    rcut: float
+
+    Return
+    ------
+    NTA arr
+    """
+    # return tc.ones(dist.shape, dtype=dist.dtype, device=dist.device)
+    return 0.25 * (tc.cos(dist * tc.pi/rcut)+1)**2
+
+
+def gauss(dist, jatn, alpha, rs):
+    """
+    dist : NN
+    sig : NS x nmax
+    rs : NS x nmax
+    return : NN x nmax
+    """
+
+    """
+    NN = len(dist)
+    dtype, device = dist.dtype, dist.device
+    gauss = tc.empty((NN, nmax), dtype=dtype, device=device)
+    for i, spe in enumerate(species):
+        # nmax
+        Sig, Rs = sigma[i], rs[i]
+        mask = (symbols == spe)
+        # NN x nmax
+        tmp = np.exp(Sig * (dist[mask, None] - Rs)**2)
+        # NN x 1
+        gauss.masked_scatter_(mask[:, None], tmp)
+    return gauss
+    """
+    # NSxnmax -> NNxnmax
+    Rs = rs[jatn]
+    alp = alpha[jatn]
+    # NN x nmax - NN x 1 -> NN x nmax
+    return tc.exp(alp * (dist[:, None] - Rs)**2)
+
+
+def angular(disp, lmax):
+    """
+    disp : NN x 3
+    lmax : int
+    return : NN x NO
+       where NN is number of total atoms
+             NO is number of orbitals, or mutinomial expansion
+
+    """
+    NN, dtype, device = len(disp), disp.dtype, disp.device
+
+    angular = []
+    multinomial = tc.ones(NN, 1, dtype=dtype, device=device)
+    angular.append(multinomial)
+
+    for loop in range(lmax):
+        # NxNOx1 x Nx1x3 -> NxNOx3 -> NxNO3
+        multinomial = (multinomial[..., None] * disp[:, None]).reshape(NN, -1)
+        angular.append(multinomial)
+
+    # NNx1 + NNx3 + NNx9 ... -> NNxNO
+    return tc.cat(angular, axis=1)
+
+
+def get_density(Wln, Csn, Fxyz, iidx, jidx, device, dtype, NTA, NO, nmax):
+    """
+    Parameters
+    ----------
+
+    Wln: NO x nmax x norb
+    Csn: NTA x nmax
+    Fxyz: NNxNOxnmax
+
+    Returns
+    -------
+     NTA x norb
+    """
+    # NTAxnmax -> NNxnmax
+    cj = Csn[jidx]
+    # NNx1xnmax x NNxNOxnmax -> NNxNOxnmax
+    cjFxyz = cj[:, None] * Fxyz
+
+    # NN x NO x nmax -> NTA x NO x nmax
+    bnl = tc.zeros((NTA, NO, nmax), device=device, dtype=dtype).index_add(
+                                            0, iidx, cjFxyz)
+
+    # NTAxNOx(nmax)x1 x 1xNOx(nmax)x(norb) -> NTAxNOx(nmax)xnorb
+    #  -> NTAxNOxnorb -> NTAxnorb
+    return tc.sum(tc.sum(bnl[..., None] * Wln[None], axis=2) ** 2, axis=1)
 
 
 class Gj(nn.Module):
@@ -206,42 +301,40 @@ class REANN(nn.Module):
       type of elements
 
     """
-    def __init__(self, species=None, rcut=6., lmax=2, nmax=2, loop=1, device='cpu',
-		 modulelist=None,
-                 **kwargs):
+    def __init__(self, species=None, rcut=6., lmax=2, nmax=2, loop=1,
+                 norb=None, device='cpu', modulelist=None, **kwargs):
         super(REANN, self).__init__()
 
         self.device = device
         self.species = species
-        #self.nmax = nmax
-        #self.lmax = lmax
-        #self.loop = loop
-        self.register_buffer('nmax', tc.Tensor([nmax]).long().to(device=device))
-        self.register_buffer('lmax', tc.Tensor([lmax]).long().to(device=device))
-        self.register_buffer('loop', tc.Tensor([loop]).long().to(device=device))
-        self.register_buffer('rcut', tc.Tensor([rcut]).to(device=device))
+        norb = norb or int((nmax + 1)*nmax*(lmax+1) / 2)
+        # Just because you can write in one line
+        malloc = self.register_buffer
+        malloc('nmax', tc.Tensor([nmax]).long().to(device=device))
+        malloc('lmax', tc.Tensor([lmax]).long().to(device=device))
+        malloc('loop', tc.Tensor([loop]).long().to(device=device))
+        malloc('norb', tc.Tensor([norb]).long().to(device=device))
+        malloc('rcut', tc.Tensor([rcut]).to(device=device))
 
         assert len(species) == max(species) + 1, "Use compressed expression"
         NS = len(species)
-        NO = int((3 ** lmax - 1) / 2)
+        NO = int((3 ** (lmax+1) - 1) / 2)
         Oidx = []
-        for i in range(lmax):
-            Oidx.extend([i] * (2*i + 1))
+        for i in range(lmax + 1):
+            Oidx.extend([i] * int(3**i))
 
-        # self.NS = NS
-        # self.NO = NO
         self.register_buffer('NS', tc.Tensor([NS]).long().to(device=device))
         self.register_buffer('NO', tc.Tensor([NO]).long().to(device=device))
-        # self.register_buffer('Oidx', tc.Tensor(Oidx).long().to(device=device))
         self.Oidx = Oidx
 
-        self.α = Parameter(-(tc.rand(NS, nmax, device=device)) - tc.log(self.rcut))
+        self.α = Parameter(-(tc.rand(NS, nmax, device=device))
+                           - tc.log(self.rcut))
         self.rs = Parameter(tc.rand(NS, nmax, device=device))
         # NS x nmax
         self.species_params = Parameter(tc.rand(NS, nmax).to(device=device))
-        # Loop x NO x nmax x NO
-        self.orbital_params = Parameter(tc.rand(nmax, NO)[None, None].repeat(
-                                  loop + 1, lmax, 1, 1).to(device=device))
+        # Loop x lmax x nmax x norb
+        self.orbital_params = Parameter(tc.rand(nmax, norb)[None, None].repeat(
+                                  loop + 1, lmax+1, 1, 1).to(device=device))
 
         if modulelist is None:
             gj = self.init_modulelist()
@@ -255,12 +348,12 @@ class REANN(nn.Module):
             descdict = nn.ModuleDict()
             for spe in self.species:
                 descdict[str(spe)] = nn.Sequential(
-                        nn.Linear(self.NO, 128),
-                         nn.LeakyReLU(),
+                        nn.Linear(self.norb, 128),
+                        nn.LeakyReLU(),
                         nn.Linear(128, self.nmax)
                 )
             modulelist.append(descdict)
-        
+
         return modulelist
 
     def forward(self, symbols, positions, cells, pbcs, energyidx, crystalidx):
@@ -280,14 +373,15 @@ class REANN(nn.Module):
 
         Returns
         -------
-        NTAx NO Tensor{Double}
+        NTAx norb Tensor{Double}
             density ρ
         """
         device = self.device
 
         # Number of crystals, Number of total atoms
         iidx, jidx, isym, jsym, disp, dist = \
-            get_neighbors_info(symbols, positions, cells, pbcs, energyidx, crystalidx, cutoff=self.rcut, device=device)
+            get_neighbors_info(symbols, positions, cells, pbcs, energyidx,
+                               crystalidx, cutoff=self.rcut, device=device)
 
         NTA = len(positions)
         dtype, device = positions.dtype, positions.device
@@ -295,126 +389,28 @@ class REANN(nn.Module):
         nmax, lmax, rcut = self.nmax, self.lmax, self.rcut
 
         # NN -> NN
-        fcut = self.cutoff_function(dist, rcut)
+        fcut = cutoff_function(dist, rcut)
         # NN -> NN
-        radial = self.gauss(dist, jsym, self.α, self.rs)
+        radial = gauss(dist, jsym, self.α, self.rs)
         # NN x NO
-        angle = self.angular(disp, lmax, NO)
-        # NNxNOx1 x NNx1xnmax -> NNxNOxnmax
+        angle = angular(disp, lmax)
+        # NNx1x1 x NNxNOx1 x NNx1xnmax -> NNxNOxnmax
         Fxyz = (fcut[..., None, None] * angle[..., None] * radial[:, None])
 
         # NN number of total neighbors
         # NSxnmax -> NTA x nmax
         Csn = self.species_params[symbols]
-        # Loop x lmax x nmax x O -> O x nmax x O
+        # Loop x lmax x nmax x norbit -> NO x nmax x norbit
         Wln = self.orbital_params[0, Oidx]
 
         params = (device, dtype, NTA, NO, nmax)
-        # NTA x O
-        ρ = self.get_density(Wln, Csn, Fxyz, iidx, jidx, *params)
+        # NTA x norb
+        ρ = get_density(Wln, Csn, Fxyz, iidx, jidx, *params)
         for i in range(self.loop):
             # NTAxO -> NTAxnmax
             Csn = Csn + self.gj[i](ρ, symbols)
-            # Loop x lmax x nmax x O -> O x nmax x O
+            # Loop x lmax x nmax x norb -> NO x nmax x norb
             Wln = self.orbital_params[i+1, Oidx]
-            # NTA x O
-            ρ = self.get_density(Wln, Csn, Fxyz, iidx, jidx, *params)
+            # NTA x norb
+            ρ = get_density(Wln, Csn, Fxyz, iidx, jidx, *params)
         return ρ
-
-    def cutoff_function(self, dist, rcut):
-        """
-        Parameters
-        ----------
-        dist: NTA
-        rcut: float
-
-        Return
-        ------
-        NTA arr
-        """
-        # return tc.ones(dist.shape, dtype=dist.dtype, device=dist.device)
-        return 0.25 * (tc.cos(dist * tc.pi/rcut)+1)**2
-
-    def gauss(self, dist, jatn, alpha, rs):
-        """
-        dist : NN
-        sig : NS x nmax
-        rs : NS x nmax
-        return : NN x nmax
-        """
-
-        """
-        NN = len(dist)
-        dtype, device = dist.dtype, dist.device
-        gauss = tc.empty((NN, nmax), dtype=dtype, device=device)
-        for i, spe in enumerate(species):
-            # nmax
-            Sig, Rs = sigma[i], rs[i]
-            mask = (symbols == spe)
-            # NN x nmax
-            tmp = np.exp(Sig * (dist[mask, None] - Rs)**2)
-            # NN x 1
-            gauss.masked_scatter_(mask[:, None], tmp)
-        return gauss
-        """
-        # NSxnmax -> NNxnmax
-        Rs = rs[jatn]
-        alp = alpha[jatn]
-        # NN x nmax - NN x 1 -> NN x nmax
-        return tc.exp(alp * (dist[:, None] - Rs)**2)
-
-    def angular(self, disp, lmax, NO):
-        """
-        unit : NTA x 3
-        disp : NTA x 3
-        fcut : NTA
-        lmax : int
-        return : NTA x O
-           where NTA is number of total atoms
-                  O is number of orbitals
-
-        Only 1, 2 support.
-        """
-        NN, dtype, device = len(disp), disp.dtype, disp.device
-
-        angular = []
-        angular.append(tc.ones(NN, 1, dtype=dtype, device=device).requires_grad_(True))
-        if lmax > 1:
-            # NNx3
-            angular.append(disp)
-        if lmax > 2:
-            pass
-            """
-            for l in range(2, lmax+1):
-                for m in range(l):
-                    Tlm = chebyshev(l, m)
-                    for pqr in mulinomial(m):
-                        p, q, r = pqr
-            """
-        # NNx1 + NNx3 ... -> NNxNO
-        return tc.cat(angular, axis=1)
-
-    def get_density(self, Wln, Csn, Fxyz, iidx, jidx, device, dtype, NTA, NO, nmax):
-        """
-        Parameters
-        ----------
-
-        Wln: O x nmax x O
-        Csn: NTA x nmax
-        Fxyz: NNxNOxnmax
-
-        Returns
-        -------
-         NTA x O
-        """
-        # NTAxnmax -> NNxnmax
-        cj = Csn[jidx]
-        # NNx1xnmax x NNxNOxnmax -> NNxNOxnmax
-        cjFxyz = cj[:, None] * Fxyz
-
-        # NN x NO x nmax -> NTA x NO x nmax
-        bnl = tc.zeros((NTA, NO, nmax), device=device, dtype=dtype).index_add(
-                                                0, iidx, cjFxyz)
-
-        # NTAxNOx(nmax)x1 x 1xNOx(nmax)xNO -> NTA x NO x nmax x NO -> NTAxOxO -> NTAxO
-        return tc.sum(tc.sum(bnl[..., None] * Wln[None], axis=2) ** 2, axis=1)
